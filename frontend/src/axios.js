4a033e3f55783236d76f01ebd7471359e8f4b7a6
bapath <harshal.bapat@jacobs.com>
2025-06-22 16:27:25 +0530

    feat: add environment configuration for API base URL and update dependencies
    - Created .env.development and .env.production files to manage API base URLs for different environments.
    - Updated package-lock.json to upgrade various dependencies, including Rollup and Vue packages, to version 4.44.0 and 3.5.17 respectively.
    - Modified axios.js to set the base URL for API requests to the production URL.

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://192.168.31.230:5000/api',
+  baseURL: 'http://192.168.31.230:5000/api',
   // baseURL: 'http://192.168.1.104:5000/api',
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
 });
 
 // Add a request interceptor to include the JWT token
